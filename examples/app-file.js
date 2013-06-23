1e721f15329d0ce4eca15863ba5bb3121c614bb9
jKey Lu <jkeylu@gmail.com>
2013-06-23 10:17:33 +0800

    Initial commit

---
+++ examples/app-file.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var connect = require('connect'),
+  SessionStore = require('../lib/connect-session-store');
+
+var options = {
+  storeType: 'file',
+  storeOptions: {
+    path:'.',
+    useAsync:true,
+    reapInterval: 5000,
+    maxAge: 10000
+  }
+};
+
+connect(
+  connect.cookieParser(),
+  connect.session({
+    secret:'session file',
+    store: SessionStore(options)
+  }),
+  connect.favicon(),
+  function(req,res,next) {
+    var sess = req.session;
+    console.log('+ begin ' + req.url );
+    if (sess.views) {
+      sess.views++;
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<p>store type: ' + options.storeType + '</p>');
+      res.write('<p>views: ' + sess.views + '</p>');
+      res.write('<p>session cookie expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+      res.end();
+    } else {
+      sess.views = 1;
+      res.end('welcome to the file session demo. refresh!');
+    }
+  }
+).listen(8080);
+
---
+++ examples/app-redis.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var connect = require('connect'),
+  SessionStore = require('../lib/connect-session-store');
+
+var options = {
+  storeType: 'redis',
+  storeOptions: {
+    ttl: 10
+  }
+};
+
+connect(
+  connect.cookieParser(),
+  connect.session({
+    secret:'session file',
+    store: SessionStore(options)
+  }),
+  connect.favicon(),
+  function(req,res,next) {
+    var sess = req.session;
+    console.log('+ begin ' + req.url );
+    if (sess.views) {
+      sess.views++;
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<p>store type: ' + options.storeType + '</p>');
+      res.write('<p>views: ' + sess.views + '</p>');
+      res.write('<p>session cookie expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+      res.end();
+    } else {
+      sess.views = 1;
+      res.end('welcome to the redis session demo. refresh!');
+    }
+  }
+).listen(8080);
+
